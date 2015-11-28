a0bf4a7f407a53ce2a665c91d3039332fa99f2c8
IOS 3501 <ios3501@Lab-G-iMac-3.local>
2015-11-28 18:09:30 +0200

    Initial Commit

---
+++ Gun7-MultiThread/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gun7-MultiThread
+//
+//  Created by IOS 3501 on 28/11/15.
+//  Copyright (c) 2015 Ali.Can. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Gun7-MultiThreadTests/Gun7_MultiThreadTests.swift
@@ -0,0 +1,36 @@
+//
+//  Gun7_MultiThreadTests.swift
+//  Gun7-MultiThreadTests
+//
+//  Created by IOS 3501 on 28/11/15.
+//  Copyright (c) 2015 Ali.Can. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Gun7_MultiThreadTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
